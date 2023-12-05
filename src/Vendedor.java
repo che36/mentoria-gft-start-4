3216aebf8ce603f64da789291fdf25128330a358
Neo40 <tron36.cc@gmail.com>
2023-12-05 16:32:04 -0300

    Criação de classe Vendedor com construtor super

---
+++ src/Vendedor.java
@@ -0,0 +1,17 @@
+public class Vendedor extends FuncionarioCLT {
+    private Double valorBonificacao;
+
+    public Vendedor(String nome, String documento, Double valorSalario, Endereco endereco) {
+        super(nome, documento, valorSalario, endereco);
+    }
+
+    @Override
+    public String toString() {
+        return "Vendedor{" +
+                "nome='" + nome + '\'' +
+                ", documento='" + documento + '\'' +
+                ", valorSalario=" + valorSalario +
+                ", endereco=" + endereco +
+                '}';
+    }
+}
