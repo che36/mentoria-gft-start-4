878a3ae192ef0ae7a1330246eb611bceda3102d8
Neo40 <tron36.cc@gmail.com>
2023-12-05 16:05:55 -0300

    Criacao de classe com 2 construtores a atributos

---
+++ src/Endereco.java
@@ -0,0 +1,14 @@
+public class Endereco {
+    private String rua;
+    private String complemento;
+    private String bairro;
+
+    public Endereco(String rua, String complemento, String bairro) {
+        this.rua = rua;
+        this.complemento = complemento;
+        this.bairro = bairro;
+    }
+
+    public Endereco() {
+    }
+}
