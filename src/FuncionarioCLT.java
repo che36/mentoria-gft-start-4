eb184c9b47a6955b4b0129e9c939f0dda6a9b846
Neo40 <tron36.cc@gmail.com>
2023-12-05 16:17:26 -0300

    Criação de construtor

--- src/FuncionarioCLT.java
+++ src/FuncionarioCLT.java
@@ -3,4 +3,11 @@ public abstract class FuncionarioCLT {
     protected String documento;
     protected Double valorSalario;
     protected Endereco endereco;
+
+    public FuncionarioCLT(String nome, String documento, Double valorSalario, Endereco endereco) {
+        this.nome = nome;
+        this.documento = documento;
+        this.valorSalario = valorSalario;
+        this.endereco = endereco;
+    }
 }
